6ecd0a0b85cb9ebac41eda1c1d784d7a8515aaf4
Yama-Tomo <mazda13bturbo@gmail.com>
2019-02-05 12:19:47 +0900

    test: added test

--- test/model/typescript/employee.ts
+++ test/model/typescript/employee.ts
@@ -4,10 +4,34 @@ import {JsonObject, JsonProperty} from "../../../src/json2typescript/json-conver
 export class Employee {
     @JsonProperty()
     id: number = 0;
-
     @JsonProperty()
     firstName: string = '';
-
     @JsonProperty('last_name')
     _lastName: string = '';
+    @JsonProperty({ type: String })
+    branchName: string = '';
+    @JsonProperty()
+    age: number = 0;
+    @JsonProperty({ optional: true })
+    hobby: string = '(optional)';
 }
+
+@JsonObject({ classIdentifier: 'invalidPropertyMapEmployee', enableAutoSnakeCaseMap: true })
+export class NoSuchPropertyEmployee {
+    @JsonProperty()
+    noSuchProp: string = '';
+}
+
+@JsonObject({ classIdentifier: 'invalidPropertyMapObjectTypeArgEmployee', enableAutoSnakeCaseMap: true })
+export class NoSuchPropertyObjectTypeArgEmployee {
+    @JsonProperty({ propName: 'no_such_prop' })
+    noSuchProp: string = '';
+}
+
+@JsonObject({ classIdentifier: 'invalidPropertyTypeEmployee', enableAutoSnakeCaseMap: true })
+export class InvalidPropertyTypeEmployee {
+    @JsonProperty({ type: Number })
+    branchName: string = '';
+}
+
+
